fe639b1c293271843a8839859c54d48c6e614842
crankbot0118 <tarunsathwikpalepu0118m2@gmail.com>
2026-04-06 08:08:19 +0000

    SOLUTION

---
+++ log_archive_too.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+
+LOG_DIR=$1
+R="\e[31m"
+G="\e[32m"
+W="\e[0m"
+log_archive(){
+    if [[ -d ${1} ]]; then
+        echo -e "File Exist Check ... ${G}SUCCESS!${W}"
+    else
+        echo -e "File Exist Check ... ${R}FAILED!${W}"
+        echo "Please check File existence and try again"
+        return 1
+    fi
+
+    DATE_NOW=$(date +%Y%m%d_%H%M%S)
+    tar -czvf "logs_archive_${DATE_NOW}.tar.gz" ${1}
+}
+
+log_archive "/home/ec2-user/Log-Archive-Tool"
